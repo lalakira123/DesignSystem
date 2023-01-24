9209f6027fc4499632093ab88f50f24b3f5beec7
Laerte <laerteakira@gmail.com>
2023-01-24 20:58:29 -0300

    feat: TextArea Component

--- packages/docs/src/stories/TextInput.stories.tsx
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -9,7 +9,14 @@ export default {
   decorators: [
     (Story) => {
       return (
-        <Box as="label" css={{ display: 'flex', flexDirection: 'column' }}>
+        <Box
+          as="label"
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '$2',
+          }}
+        >
           <Text size="sm">Email address</Text>
           {Story()}
         </Box>
