f954a78d6857e0c76505697f40e7ae696f8f7228
Laerte <laerteakira@gmail.com>
2023-01-24 09:13:29 -0300

    feat: Box Component

---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, BoxProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Surfaces/Box',
+  component: Box,
+
+  args: {
+    children: (
+      <>
+        <span>Testa elemento box</span>
+      </>
+    )
+  },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {
+  
+}
+
